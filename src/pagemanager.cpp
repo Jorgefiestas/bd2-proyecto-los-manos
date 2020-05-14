8eedac179b8f2cb14013f8ba044ce16033b6249a
Jorge Ignacio Fiestas Lavezzolo <jorge.fiestas@utec.edu.pe>
2020-05-14 11:03:14 -0500

    feat(generator): Adds tests generator for generating large test cases

--- src/pagemanager.cpp
+++ src/pagemanager.cpp
@@ -1,38 +1,37 @@
 #include "../include/pagemanager.hpp"
 
-pagemanager::pagemanager(std::string file_name, bool trunc)
-    : std::fstream(file_name.data(),
-                   std::ios::in | std::ios::out | std::ios::binary) {
-  empty = false;
-  fileName = file_name;
-  if (!good() || trunc) {
-    empty = true;
-    open(file_name.data(),
-         std::ios::in | std::ios::out | std::ios::trunc | std::ios::binary);
-  }
+pagemanager::pagemanager(std::string file_name, bool trunc): 
+	std::fstream(file_name.data(), std::ios::in | std::ios::out | std::ios::binary) {
+	empty = false;
+	fileName = file_name;
+	if (!good() || trunc) {
+		empty = true;
+		open(file_name.data(),
+				 std::ios::in | std::ios::out | std::ios::trunc | std::ios::binary);
+	}
 }
 
 pagemanager::~pagemanager() { close(); }
 
 template <class Register> 
 void pagemanager::save(const long &n, Register &reg) {
-    clear();
-    seekp(n * sizeof(Register), std::ios::beg);
-    write(reinterpret_cast<const char *>(&reg), sizeof(reg));
+		clear();
+		seekp(n * sizeof(Register), std::ios::beg);
+		write(reinterpret_cast<const char *>(&reg), sizeof(reg));
 }
 template <class Register>
 bool pagemanager::recover(const long &n, Register &reg) {
-    clear();
-    seekg(n * sizeof(Register), std::ios::beg);
-    read(reinterpret_cast<char *>(&reg), sizeof(reg));
-    return gcount() > 0;
+		clear();
+		seekg(n * sizeof(Register), std::ios::beg);
+		read(reinterpret_cast<char *>(&reg), sizeof(reg));
+		return gcount() > 0;
 }
 
 template <class Register>
 void pagemanager::erase(const long &n) {
-    char mark;
-    clear();
-    mark = 'N';
-    seekg(n * sizeof(Register), std::ios::beg);
-    write(&mark, 1);
+		char mark;
+		clear();
+		mark = 'N';
+		seekg(n * sizeof(Register), std::ios::beg);
+		write(&mark, 1);
 }
