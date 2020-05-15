6b64b4a93df25ce3c1a0bb264563554e333f9219
Martin Carrasco <martin.carrasco@utec.edu.pe>
2020-05-14 23:10:51 -0500

    Reestructured project because of linker errors, Added Person comparison, Added testing for B+ tree access to generated files

--- include/hash/dynamic_hash.hpp
+++ include/hash/dynamic_hash.hpp
@@ -1,3 +1,6 @@
+#ifndef DYNAMIC_HASH_HPP
+#define DYNAMIC_HASH_HPP
+
 #include <list>
 #include <map>
 #include <memory>
@@ -5,19 +8,19 @@
 #include <stdexcept>
 #include <vector>
 
-template <class T, int fd> struct Bucket {
+template <class T, int fd> struct BaseBucket {
   std::map<int, T> values;
   std::vector<T> overflow;
   int depth;
   int size;
 
-  Bucket(int depth, int size);
+  BaseBucket(int depth, int size);
   int insert(int key, T obj, bool force);
   std::optional<T> search(int key);
 };
 
 template <class T, int fd = 2> class DinHash {
-  using Bucket = Bucket<T, fd>;
+  using Bucket = BaseBucket<T, fd>;
   int max_depth;
   std::list<std::shared_ptr<Bucket>> buckets;
   unsigned int make_mask(int n) { return (1 << n) - 1; }
@@ -32,3 +35,69 @@ public:
   std::optional<T> search(int key);
   bool split_bucket(int hash, const T &obj);
 };
+#endif
+template <class T, int fd> BaseBucket<T, fd>::BaseBucket(int depth, int size) {
+  depth = depth;
+  size = size;
+}
+
+template <class T, int fd> int BaseBucket<T, fd>::insert(int key, T obj, bool force) {
+  if (values.size() == size and not force)
+    return 0;
+  if (values.size() == size and force) {
+    overflow.push_back(obj);
+    return 0;
+  }
+  values[key] = obj;
+  return 1;
+}
+template <class T, int fd> std::optional<T> BaseBucket<T, fd>::search(int key) {
+
+  auto it = values.find(key);
+  if (it != values.end())
+    return std::make_optional(values[key]);
+  return std::nullopt;
+}
+// Dynamic Hash
+
+template <class T, int fd> void DinHash<T, fd>::insert(int key, T obj) {
+  unsigned int hash = obj & make_mask(max_depth);
+  int result = buckets[hash]->insert(hash, obj);
+  if (result == 1 || result == 0)
+    return;
+  bool force = split_bucket(hash, obj);
+  insert(key, obj, force);
+}
+
+template <class T, int fd> std::optional<T> DinHash<T, fd>::search(int key) {
+  unsigned int hash = key & make_mask(max_depth);
+  return buckets[hash]->search(key);
+}
+template <class T, int fd>
+bool DinHash<T, fd>::split_bucket(int hash, const T &obj) {
+  auto buck = buckets[hash];
+  if (buck->depth == max_depth) {
+    return false;
+  }
+  buck->depth++;
+
+  // Copy values from bucket and clear
+  auto vec_copy = buck->values.copy();
+  vec_copy[hash] = obj;
+  buck->values.clear();
+
+  // Create new buckets if they don't exist
+  if (buckets.size() != pow(2, buck->depth)) {
+    for (int x = (1 << buck->depth) - 1; x > (1 << (buck->depth - 1)) - 1;
+         x--) {
+      buckets[x] = nullptr;
+    }
+    buckets.resize(pow(2, buck->depth));
+  }
+
+  int pairHash = (1 << (buck->depth - 1)) | hash;
+  for (const auto &[k, v] : vec_copy) {
+    buckets[k]->insert(k, v);
+  }
+  return true;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,82 @@
+#include <bplus/pagemanager.hpp>
+#include <hash/dynamic_hash.hpp>
 #include <bplus/bptree.hpp>
 #include <random_file/random_file.hpp>
+#include <iostream>
+#include <chrono>
+#include <memory>
+#include <fstream>
+#include <string>
 
-int main(){
 
-	return 0;
+struct Person {
+  int dni;
+  char name[20];
+  char lastname[20];
+  int age;
+  char birth_date[10];
+
+	bool operator <(const Person& p){
+		return dni < p.dni;
+	}
+
+  friend std::ostream &operator<<(std::ostream& stream, Person& record) {
+    stream.write((char *)&record.dni, sizeof(int));
+    stream.write((char *)&record.name, 20);
+    stream.write((char *)&record.lastname, 20);
+    stream.write((char *)&record.age, sizeof(int));
+    stream.write((char*)&record.birth_date, sizeof(int));
+
+    stream << "\n";
+    stream << std::flush;
+    return stream;
+  }
+
+  friend std::istream &operator>>(std::istream &stream, Person& record) {
+    stream.read((char*)&record.dni, sizeof(int));
+    stream.read((char*)&record.name, 20);
+    stream.read((char*)&record.lastname, 20);
+    stream.read((char*)&record.age, sizeof(int));
+    stream.read((char*)&record.birth_date, sizeof(int));
+    stream.get();
+    return stream;
+  }
+};
+
+
+void btree_test(const std::string& dataset1, const std::string& dataset2) {
+	auto pm_ptr_1 = std::make_shared<pagemanager>("btree_data_1.bin");
+	auto pm_ptr_2 = std::make_shared<pagemanager>("btree_data_2.bin");
+	btree<Person> tree_1(std::move(pm_ptr_1));
+	btree<Person> tree_2(std::move(pm_ptr_2));
+
+	std::ifstream stream_1(dataset1);
+	
+	Person tmp;
+	while (not stream_1.eof()){
+		stream_1 >> tmp;
+		tree_1.insert(tmp);
+	}
+		
+	//TODO LOAD FILE WITH DATA	
+	//TODO INSERT IN TREE	
+}
+void insert_dyn_hash() {
+
+}
+void insert_random_file() {}
+
+int main() {
+	const std::string data_1 = "../generator/register-dataset.txt";
+	const std::string data_2 = "../generator/register-dataset.txt";
+
+
+	// Time for B+
+	auto start = std::chrono::steady_clock::now();
+	btree_test(data_1, data_2);
+	auto end = std::chrono::steady_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	std::cout << "Took "  << duration << "ms" << std::endl;
+
+	return 0; 
 }
